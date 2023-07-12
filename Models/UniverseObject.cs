2f503b11d4c6e60bb71cb4fe65f3c69433eb3476
Damian Caboń <damian.cabon@icloud.com>
2023-07-12 18:46:38 +0200

    test

--- Models/UniverseObject.cs
+++ Models/UniverseObject.cs
@@ -1,4 +1,5 @@
 
+using System.Diagnostics;
 using GalaxyMap.Utilities;
 
 namespace GalaxyMap.Models
@@ -7,25 +8,37 @@ namespace GalaxyMap.Models
 	{
 		public double MassKg { get; private set; }
 		public string? Name { get; private set; }
-		public double AUDistance { get; private set; }
+		public double LightYearsDistanceFromEarth { get; private set; }
 		public double RadiusMeters { get; private set; }
 		public UniverseObjectType UniverseObjectType { get; protected set; }
 		public UniverseObject(
 			string name,
 			double massKg = -1,
 			double radiusMeters = -1,
-			double aUDistance = -1
+			double lightYearsDistanceFromEarth = -1
 			)
 		{
 			Name = name is null ? throw new ArgumentNullException(nameof(name)) : name;
 			MassKg = massKg;
 			RadiusMeters = radiusMeters;
-			AUDistance = aUDistance;
+			LightYearsDistanceFromEarth = lightYearsDistanceFromEarth;
 		}
 		public double GravityForceN()
 		{
+			if (MassKg != -1 && RadiusMeters != -1)
+			{
+				Trace.WriteLine($"Cant properly calculate GravityForvce (N) with given Values : {nameof(MassKg)}:{MassKg} {nameof(RadiusMeters)}:{RadiusMeters}");
+				return 0;
+			}
 			return (PhysicalConstants.G * MassKg) / Math.Pow(RadiusMeters, 2);
 		}
+		// public double GravityForceNToObject(UniverseObject universeObject)
+		// {
+		// 	if (MassKg != -1 && RadiusMeters != -1 && LightYearsDistanceFromEarth != -1)
+		// 	{
+
+		// 	}
+		// }
 		public double GravityForceNDiff(UniverseObject universeObject)
 		{
 			return MathService.DivideByZero(this.GravityForceN(), universeObject.GravityForceN());
@@ -34,5 +47,6 @@ namespace GalaxyMap.Models
 		{
 			return MathService.DivideByZero(universeObjectFirst.GravityForceN(), universeObjectSecond.GravityForceN());
 		}
+
 	}
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,46 @@
-﻿using GalaxyMap.Models;
+﻿using System.Runtime.CompilerServices;
+using System.Diagnostics.Tracing;
+using System.ComponentModel;
+using System.Security.AccessControl;
+using System.Security.Cryptography.X509Certificates;
+using System.Globalization;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using Serilog;
+using Serilog.Events;
+
+
+using GalaxyMap.Models;
 using GalaxyMap.Utilities;
 
-// Console.WriteLine("Hello, World!");
 
-Planet earth = new("Earth", 5.972e24, 6371e3);
+Planet earth = new("Earth", 5.972e24, 6372e3, 0);
 BlackHole sagittarius = new("Sagittarius A*", 8.26e36, 12e6);
+string imagesDir = Path.Combine(Environment.CurrentDirectory, "images");
+IEnumerable<string> images = Directory.EnumerateFiles(imagesDir);
+foreach (string pathOfImage in images)
+{
+	string pathThumbnail = Path.Combine(Environment.CurrentDirectory, "images", Path.GetFileNameWithoutExtension(pathOfImage) + "-thumbnail" + Path.GetExtension(pathOfImage));
+	using (Image image = Image.Load(pathOfImage))
+	{
+		image.Mutate(x => x.Resize(image.Width / 10, image.Height / 10));
+		image.Mutate(x => x.Grayscale());
+		image.Save(pathThumbnail);
+	}
+}
+System.Console.WriteLine("Images processing finished.");
+
+Log.Logger = new LoggerConfiguration()
+				.MinimumLevel.Debug()
+				.WriteTo.Console()
+				.Enrich.FromLogContext()
+				.WriteTo.File(Path.Combine(Environment.CurrentDirectory, "log.txt"), rollingInterval: RollingInterval.Day)
+				.CreateLogger();
+
+
+
+
+
+
+
+
